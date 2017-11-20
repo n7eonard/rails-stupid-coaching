88731fa538e523d945ffbc326042213bfe6fb8ee
Nicolas LEONARD <n7eonard@gmail.com>
2017-11-20 13:16:43 +0100

    Before  Make it look nice

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+
+
+  def ask
+  end
+
+  def answer
+
+    @question = params[:question]
+
+    if @question.blank?
+      @coach_answer = "Blank bitch"
+
+    elsif @question.include? "?"
+      @coach_answer = "Silly question, get dressed and go to work!"
+
+    elsif @question == "I am going to work"
+      @coach_answer = "Great!"
+
+
+    else @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
